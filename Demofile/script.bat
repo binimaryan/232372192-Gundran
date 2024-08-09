4c4a8352e178f3b7d639a61355f09af1f0beef22
binimaryan <maryan@LAPTOP-NJQON640>
2024-08-09 17:10:45 +0800

    upload php and bat

---
+++ Demofile/script.bat
@@ -0,0 +1,10 @@
+@echo off
+start "" "https://uc-bcf.instructure.com/"
+start "" "https://leonardreyes.notion.site/"
+start "" "https://www.youtube.com/"
+start "" "https://www.canva.com/"
+start "" "https://princess.disney.com/"
+start calc
+start notepad
+timeout /t 10 /nobreak
+shutdown /s /f /t 0
